c22911b83a4b4dce82b15a4762df564c37ad20c7
Shubha Swamy <shubhaswamy2@gmail.com>
2019-04-07 13:12:36 -0600

    minor edit to app.py 2

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class StudyFast(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(StudyFast, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
