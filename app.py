b23a1067638e4d1cb9dce5a47fd4b7c1075b4432
Shubha Swamy <shubhaswamy2@gmail.com>
2019-04-07 13:26:07 -0600

    added v1 search endpoint

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
 api = Api(app)
 
-class StudyFast(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
-api.add_resource(StudyFast, '/')
+@app.route('/', methods=['GET'])
+def home():
+	rV = {"home": "test"}
+	return jsonify(rV)
+
+
+@app.route('/api/v1/search', methods=['GET'])
+def search():
+	retVal = {"hello": "world"}
+	return jsonify(retVal)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=65010)
+
+
